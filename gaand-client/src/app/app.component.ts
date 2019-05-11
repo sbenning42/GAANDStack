7929224a767d668d68579ff32af4bba10cc42901
unknown <sbenning@student.42.fr>
2019-05-11 21:39:05 +0200

    todos

--- gaand-client/src/app/app.component.ts
+++ gaand-client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HelloGQL, Hello } from './graphql';
+import { HelloGQL, Hello, TodoGQL, Todo } from './graphql';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -11,11 +11,18 @@ import { map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   title = 'app';
   hello$: Observable<string>;
-  constructor(private helloGql: HelloGQL) {}
+  todos$: Observable<Todo.Todo[]>;
+  constructor(
+    private helloGQL: HelloGQL,
+    private todoGQL: TodoGQL,
+  ) {}
 
   ngOnInit() {
-    this.hello$ = this.helloGql.watch().valueChanges.pipe(
+    this.hello$ = this.helloGQL.watch().valueChanges.pipe(
       map(v => v.data && v.data.hello)
+    );
+    this.todos$ = this.todoGQL.watch({ first: 25 }).valueChanges.pipe(
+      map(v => v.data && v.data.Todo)
     );
   }
 }
--- gaand-client/src/app/graphql/index.ts
+++ gaand-client/src/app/graphql/index.ts
@@ -1,5 +1,55 @@
 export type Maybe<T> = T | null;
 
+export interface _TodoFilter {
+  AND?: Maybe<(Maybe<_TodoFilter>)[]>;
+
+  OR?: Maybe<(Maybe<_TodoFilter>)[]>;
+
+  id?: Maybe<string>;
+
+  id_not?: Maybe<string>;
+
+  id_in?: Maybe<string[]>;
+
+  id_not_in?: Maybe<string[]>;
+
+  id_contains?: Maybe<string>;
+
+  id_not_contains?: Maybe<string>;
+
+  id_starts_with?: Maybe<string>;
+
+  id_not_starts_with?: Maybe<string>;
+
+  id_ends_with?: Maybe<string>;
+
+  id_not_ends_with?: Maybe<string>;
+
+  name?: Maybe<string>;
+
+  name_not?: Maybe<string>;
+
+  name_in?: Maybe<string[]>;
+
+  name_not_in?: Maybe<string[]>;
+
+  name_contains?: Maybe<string>;
+
+  name_not_contains?: Maybe<string>;
+
+  name_starts_with?: Maybe<string>;
+
+  name_not_starts_with?: Maybe<string>;
+
+  name_ends_with?: Maybe<string>;
+
+  name_not_ends_with?: Maybe<string>;
+
+  done?: Maybe<boolean>;
+
+  done_not?: Maybe<boolean>;
+}
+
 export interface _Neo4jDateInput {
   year?: Maybe<number>;
 
@@ -90,6 +140,21 @@ export interface _Neo4jTimeInput {
   formatted?: Maybe<string>;
 }
 
+export interface _TodoInput {
+  id: string;
+}
+
+export enum _TodoOrdering {
+  IdAsc = "id_asc",
+  IdDesc = "id_desc",
+  NameAsc = "name_asc",
+  NameDesc = "name_desc",
+  DoneAsc = "done_asc",
+  DoneDesc = "done_desc",
+  _IdAsc = "_id_asc",
+  _IdDesc = "_id_desc"
+}
+
 export enum _RelationDirections {
   In = "IN",
   Out = "OUT"
@@ -106,6 +171,35 @@ export namespace Hello {
     __typename?: "Query";
 
     hello: string;
+  };
+}
+
+export namespace Todo {
+  export type Variables = {
+    id?: Maybe<string>;
+    name?: Maybe<string>;
+    done?: Maybe<boolean>;
+    _id?: Maybe<string>;
+    first?: Maybe<number>;
+    offset?: Maybe<number>;
+    orderBy?: Maybe<(Maybe<_TodoOrdering>)[]>;
+    filter?: Maybe<_TodoFilter>;
+  };
+
+  export type Query = {
+    __typename?: "Query";
+
+    Todo: Maybe<(Maybe<Todo>)[]>;
+  };
+
+  export type Todo = {
+    __typename?: "Todo";
+
+    id: string;
+
+    name: string;
+
+    done: boolean;
   };
 }
 
@@ -129,6 +223,38 @@ export class HelloGQL extends Apollo.Query<Hello.Query, Hello.Variables> {
   document: any = gql`
     query Hello {
       hello
+    }
+  `;
+}
+@Injectable({
+  providedIn: "root"
+})
+export class TodoGQL extends Apollo.Query<Todo.Query, Todo.Variables> {
+  document: any = gql`
+    query Todo(
+      $id: ID
+      $name: String
+      $done: Boolean
+      $_id: String
+      $first: Int
+      $offset: Int
+      $orderBy: [_TodoOrdering]
+      $filter: _TodoFilter
+    ) {
+      Todo(
+        id: $id
+        name: $name
+        done: $done
+        _id: $_id
+        first: $first
+        offset: $offset
+        orderBy: $orderBy
+        filter: $filter
+      ) {
+        id
+        name
+        done
+      }
     }
   `;
 }
